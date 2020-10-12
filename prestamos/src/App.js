e794dc9d811f79353c9dd9bb36a8ef9945917f2d
Gustavo Velazquez <gabrivelaz@hotmail.com>
2020-10-12 17:52:28 -0300

    Utilizando el componente y reutilizando la misma, tambien utilizamos fragment

--- prestamos/src/App.js
+++ prestamos/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-
+import React, {Fragment} from 'react';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hola Mundo</h1>
-    </div>
+    <Fragment>
+        <Header/>
+        <Header/>
+        <Header/>
+        <Header/>
+    </Fragment>
   );
 }
 
